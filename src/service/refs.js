78e6b9f22cc0b8d40d0d0091540d5bbeef3795b9
AleksandrKatsinskiy <katsinskiy@gmail.com>
2022-09-24 22:58:16 +0200

    hw2

---
+++ src/service/refs.js
@@ -0,0 +1,7 @@
+export default {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('input'),
+  targetElement: document.querySelector('.target-element'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+};
